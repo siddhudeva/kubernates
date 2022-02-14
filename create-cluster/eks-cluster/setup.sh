d55f63c57250d0d69f810fa0b3c1a685827ca00c
siddhudeva <siddhudangeti@gmail.com>
2022-02-14 17:25:47 +0530

    kubernates minikube creation

---
+++ create-cluster/eks-cluster/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/.kube
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/k8-client-stack/install.sh | sudo bash
+curl https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/eksctl/install.sh | sudo bash
+
+eksctl create cluster --name sample2 --region us-east-1 --managed --zones us-east-1a,us-east-1b
